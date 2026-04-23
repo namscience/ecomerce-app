c5469662f3df3981014bd414f0f12a9f8913f8b9
namscience <nguyenkimhoangnam23@gmail.com>
2026-04-23 18:10:23 +0800

    cart screen UI completed

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -3,10 +3,13 @@ import React from 'react'
 import { Tabs } from "expo-router"
 import { Ionicons, Feather } from "@expo/vector-icons"
 import { COLORS } from '@/constants'
+import { useCart } from '@/context/CartContext'
 
 
 
 export default function TabLayout() {
+
+    const {cartItems} = useCart()
   return (
     <Tabs 
     screenOptions={{
@@ -28,9 +31,19 @@ export default function TabLayout() {
         }}/>
 
         <Tabs.Screen name = "cart" options={{
-            tabBarIcon: ({color, focused}) => <Feather name=
-            {focused ? "shopping-cart" : "shopping-cart"} size={26} 
-            color={color} />
+            tabBarIcon: ({color, focused}) => (
+                <View className = 'relative'>
+                    <Feather name=
+                    {focused ? "shopping-cart" : "shopping-cart"} size={26} 
+                    color={color} />
+
+                    {cartItems?.length > 0 && 
+                    <View className='absolute -top-2 -right-2 bg-accent size-3 rounded-full items-center justify-center'>
+                        <Ionicons name = 'ellipse' size = {6} color = 'white'/>
+                    </View>
+        }
+                </View>
+            )
         }}/>
 
         <Tabs.Screen name = "favorites" options={{
--- client/app/(tabs)/cart.tsx
+++ client/app/(tabs)/cart.tsx
@@ -1,10 +1,70 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useCart } from '@/context/CartContext'
+import { useRouter } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Header from '@/components/Header'
+import { ScrollView } from 'react-native-gesture-handler'
+import CartItem from '@/components/CartItem'
 
 export default function Cart() {
+  const { cartItems, cartTotal, removeFromCart, updateQuantity } = useCart()
+  const router = useRouter()
+
+  const shipping = 2.00;
+  const total = cartTotal + shipping;
+
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-surface' edges={['top']}>
+      <Header title="My Cart" showBack/>
+
+      {cartItems.length > 0 ? (
+        <>
+        <ScrollView className='flex-1' style={{ paddingHorizontal: 16, marginTop: 16 }}>
+          {cartItems.map((item, index)=>(
+            <CartItem key={index}
+            item={item}
+            onRemove={()=>removeFromCart(item.id, item.size)}
+            onUpdateQuantity={(q)=>updateQuantity(item.id, q, item.size)} />
+          ))}
+        </ScrollView>
+
+        <View className='bg-white rounded-t-3xl shadow-sm' style={{ padding: 16 }}>
+          {/* Subtotal */}
+          <View className = 'flex-row justify-between mb-2'>
+            <Text className="text-secondary">Subtotal</Text>
+            <Text className="text-primary font-bold">${cartTotal.toFixed(2)}</Text>
+          </View>
+
+          {/* Shipping */}
+          <View className = 'flex-row justify-between mb-2'>
+            <Text className="text-secondary">Shipping</Text>
+            <Text className="text-primary font-bold">${shipping.toFixed(2)}</Text>
+          </View>
+
+          {/* border */}
+          <View className = 'h-[1px] bg-border mb-4' />
+
+          {/* Total */}
+          <View className = 'flex-row justify-between mb-2'>
+            <Text className="text-primary font-bold text-lg">Total</Text>
+            <Text className="text-primary font-bold">${total.toFixed(2)}</Text>
+          </View>
+
+          {/* Checkout button */}
+          <TouchableOpacity className = 'bg-primary py-4 rounded-full items-center' onPress = {()=>router.push('/checkout')}>
+            <Text className='text-white font-bold text-base'>Checkout</Text>
+          </TouchableOpacity>
+        </View>
+        </>
+      ) : (
+        <View className='flex-1 items-center justify-center'>
+          <Text className='text-secondary text-lg'> Your cart is empty </Text>
+          <TouchableOpacity onPress={()=> router.push('/')} style={{ marginTop: 16 }}>
+            <Text className='text-primary font-bold'> Start Shopping </Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </SafeAreaView>
   )
 }
